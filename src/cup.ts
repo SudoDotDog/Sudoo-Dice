9db11796ce87d28014ad66f56454823ab6f6f598
WMXPY <wm@mengw.io>
2021-07-01 17:12:51 -0500

    add rollall and initial config

--- src/cup.ts
+++ src/cup.ts
@@ -8,15 +8,38 @@ import { IDice } from "./declare";
 
 export class DiceCup {
 
-    public static create(dices: IDice[]): DiceCup {
+    public static create(dices: IDice[], initialRoll: boolean = true): DiceCup {
 
-        return new DiceCup([...dices]);
+        return new DiceCup([...dices], initialRoll);
     }
 
     private readonly _dices: IDice[];
 
-    private constructor(dices: IDice[]) {
+    private constructor(dices: IDice[], initialRoll: boolean) {
 
         this._dices = dices;
+
+        if (initialRoll) {
+            this.rollAll();
+        }
+    }
+
+    public rollAll(): this {
+
+        this._dices.forEach((dice: IDice) => {
+            dice.roll();
+        });
+
+        return this;
+    }
+
+    public clone(initialRoll: boolean = false): DiceCup {
+
+        const newDices: IDice[] = this._dices.map((dice: IDice) => {
+            return dice.clone();
+        });
+
+        const newCup: DiceCup = new DiceCup(newDices, initialRoll);
+        return newCup;
     }
 }
--- src/dice.ts
+++ src/dice.ts
@@ -8,19 +8,19 @@ import { IDice } from "./declare";
 
 export class Dice implements IDice {
 
-    public static faces(faces: number): Dice {
+    public static faces(faces: number, initialRoll: boolean = true): Dice {
 
-        return new Dice(1, faces);
+        return new Dice(1, faces, initialRoll);
     }
 
-    public static minmax(min: number, max: number): Dice {
+    public static minmax(min: number, max: number, initialRoll: boolean = true): Dice {
 
-        return new Dice(min, max);
+        return new Dice(min, max, initialRoll);
     }
 
-    public static cube(): Dice {
+    public static cube(initialRoll: boolean = true): Dice {
 
-        return Dice.faces(6);
+        return Dice.faces(6, initialRoll);
     }
 
     private readonly _min: number;
@@ -28,10 +28,16 @@ export class Dice implements IDice {
 
     private _current: number;
 
-    private constructor(min: number, max: number) {
+    private constructor(min: number, max: number, initialRoll: boolean) {
 
         this._min = Math.floor(min);
         this._max = Math.floor(max);
+
+        if (initialRoll) {
+            this._current = this._getRollValue();
+        } else {
+            this._current = this._min;
+        }
     }
 
     public get current(): number {
@@ -40,24 +46,37 @@ export class Dice implements IDice {
 
     public roll(): number {
 
-        const randomNumber: number = Math.random();
-        const range: number = this._max - this._min;
-
-        const extendValue: number = Math.floor(randomNumber * range);
-        const result: number = extendValue + this._min;
-
+        const result: number = this._getRollValue();
         this._setCurrent(result);
         return result;
     }
 
-    public clone(): Dice {
+    public clone(initialRoll: boolean = false): Dice {
 
-        return new Dice(this._min, this._max);
+        const newDice: Dice = new Dice(this._min, this._max, false);
+
+        if (initialRoll) {
+            newDice.roll();
+        } else {
+            newDice._setCurrent(this._current);
+        }
+        return newDice;
     }
 
     protected _setCurrent(current: number): this {
 
         this._current = current;
         return this;
+    }
+
+    private _getRollValue(): number {
+
+        const randomNumber: number = Math.random();
+        const range: number = this._max - this._min;
+
+        const extendValue: number = Math.floor(randomNumber * range);
+        const result: number = extendValue + this._min;
+
+        return result;
     }
 }
